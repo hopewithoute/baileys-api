162fdf5aeeb759a09ae2a80690399ede4e06074f
hopewithoute <anggi.wibiyanto@gmail.com>
2023-03-19 04:55:31 +0700

    Add JWT Shared Auth

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,15 @@ import 'dotenv/config';
 import express from 'express';
 import routes from './routes';
 import { init } from './wa';
+import { expressjwt } from "express-jwt";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use('/', routes);
+app.use('/',
+    expressjwt({ secret: process.env.SHARED_SECRET || "Another Secret", algorithms: ["HS256"] }),
+    routes
+);
 app.all('*', (req, res) => res.status(404).json({ error: 'URL not found' }));
 
 const host = process.env.HOST || '0.0.0.0';
